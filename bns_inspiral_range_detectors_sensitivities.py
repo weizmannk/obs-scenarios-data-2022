cf679e8c4e155ac6f12381a69973094e6c52e8e5
Weizmann <kiend.weizman7@gmail.com>
2023-12-02 10:59:36 +0100

    add O4a data

--- bns_inspiral_range_detectors_sensitivities.py
+++ bns_inspiral_range_detectors_sensitivities.py
@@ -24,7 +24,7 @@ from matplotlib import pyplot
 pyplot.rc('axes', axisbelow=True)
 
 
-colors = {'L1': '#4ba6ff', 'V1': '#ee0000', 'K1': '#9b59b6'}
+colors = {'L1': '#4ba6ff', 'V1': '#ee0000', 'H1': '#9b59b6'}
 
 
 
@@ -36,7 +36,7 @@ if not os.path.isdir(outdir):
 path = Path('ASD')
 
 
-run_names = run_dirs=  ['O4', 'O5']
+run_names = run_dirs=  ['O4'] #, 'O5']
 
 # Sensitivities files
 #O4= ['aligo_O4high.txt', 'avirgo_O4high_NEW.txt', 'kagra_10Mpc.txt']
@@ -47,9 +47,13 @@ run_names = run_dirs=  ['O4', 'O5']
 print("======================================")
 for run_name in run_names:
     if run_name =='O4':
-       freq_L1, asd_L1     = np.loadtxt(f'{path}/aligo_O4high.txt', unpack=True)
-       freq_V1, asd_V1     = np.loadtxt(f'{path}/avirgo_O4high_NEW.txt', unpack=True)
-       freq_K1, asd_K1     = np.loadtxt(f'{path}/kagra_10Mpc.txt', unpack=True)
+       #freq_L1, asd_L1     = np.loadtxt(f'{path}/aligo_O4high.txt', unpack=True)
+       #freq_V1, asd_V1     = np.loadtxt(f'{path}/avirgo_O4high_NEW.txt', unpack=True)
+       freq_L1, asd_L1     = np.loadtxt(f'{path}/asd_O4a_messured_L1.txt', unpack=True)
+       freq_V1, asd_V1     = np.loadtxt(f'{path}/asd_O4a_Virgo_78.txt', unpack=True)
+       freq_H1, asd_H1     = np.loadtxt(f'{path}/asd_O4a_mesured_H1.txt', unpack=True)
+       
+       #freq_K1, asd_K1     = np.loadtxt(f'{path}/kagra_10Mpc.txt', unpack=True)
     else:
         freq_L1, asd_L1    = np.loadtxt(f'{path}/AplusDesign.txt', unpack=True)
         freq_V1, asd_V1    = np.loadtxt(f'{path}/avirgo_O5low_NEW.txt', unpack=True)
@@ -67,10 +71,12 @@ for run_name in run_names:
 
 
     # Plot amplitude spectral density of the detectors
-    fig = pyplot.figure(figsize=(3.375, 3))
-    pyplot.loglog(freq_L1, asd_L1, label=r'$\mathrm{LIGO}\,\mathrm{H1-L1}$', color=colors['L1'], linewidth=1, alpha=0.7)
+    fig = pyplot.figure(figsize=(4.5, 3))
+    pyplot.loglog(freq_L1, asd_L1, label=r'$\mathrm{LIGO}\,\mathrm{L1}$', color=colors['L1'], linewidth=1, alpha=0.7)
+    pyplot.loglog(freq_H1, asd_H1, label=r'$\mathrm{LIGO}\,\mathrm{H1}$', color=colors['H1'], linewidth=1, alpha=0.7)
+
     pyplot.loglog(freq_V1, asd_V1, label=r'$\mathrm{Virgo}$', color=colors['V1'], linewidth=1, alpha=0.7)
-    pyplot.loglog(freq_K1, asd_K1, label=r'$\mathrm{KAGRA}$', color=colors['K1'], linewidth=1, alpha=0.7)
+    #pyplot.loglog(freq_K1, asd_K1, label=r'$\mathrm{KAGRA}$', color=colors['K1'], linewidth=1, alpha=0.7)
 
     pyplot.legend(loc=(0.065, 0.73))
     pyplot.xlabel(r'$\mathrm{Frequency}\,\mathrm{[Hz]}$')
@@ -80,7 +86,7 @@ for run_name in run_names:
 
     pyplot.grid()
     fig.tight_layout()
-    plt.savefig(f'{outdir}/Strain_{run_name}.png', dpi=300)
+    plt.savefig(f'{outdir}/Strain_{run_name}_HLV.png', dpi=300)
     plt.close()
     
     
@@ -89,16 +95,17 @@ for run_name in run_names:
 
     range_L1 = inspiral_range(FrequencySeries(asd_L1**2, f0=freq_L1[0], df=freq_L1[1]-freq_L1[0]), snr =8, fmin=10, mass1=1.4, mass2=1.4).value
     range_V1 = inspiral_range(FrequencySeries(asd_V1**2, f0=freq_V1[0], df=freq_V1[1]-freq_V1[0]), snr =8, fmin=10, mass1=1.4, mass2=1.4).value
-    range_K1 = inspiral_range(FrequencySeries(asd_K1**2, f0=freq_K1[0], df=freq_K1[1]-freq_K1[0]), snr =8, fmin=1,  mass1=1.4, mass2=1.4).value
-
+    #range_K1 = inspiral_range(FrequencySeries(asd_K1**2, f0=freq_K1[0], df=freq_K1[1]-freq_K1[0]), snr =8, fmin=1,  mass1=1.4, mass2=1.4).value
+    range_H1 = inspiral_range(FrequencySeries(asd_H1**2, f0=freq_H1[0], df=freq_L1[1]-freq_L1[0]), snr =8, fmin=10, mass1=1.4, mass2=1.4).value
 
     print(f"\nThe BNS range in  Run {run_name}")
     print("** mass1=1.4, mass2=1.4, SNR=8 **\n")
     print(" inspiral sensitive distance\n")
-    print(f"LIGO L1-H1 range : {np.round(range_L1, 0)} Mpc")
-    print(f"Virgo range      : {np.round(range_V1, 0)} Mpc")
-    print(f"KAGRA range      : {np.round(range_K1, 0)} Mpc\n")
+    print(f"LIGO L1 range      : {np.round(range_L1, 0)} Mpc")
+    print(f"Virgo range        : {np.round(range_V1, 0)} Mpc")
+    print(f"LIGO H1 range      : {np.round(range_H1, 0)} Mpc\n")
+    #print(f"KAGRA range      : {np.round(range_K1, 0)} Mpc\n")
     print("======================================")
     
-    del  freq_L1, asd_L1, freq_V1, asd_V1, freq_K1, asd_K1 
+    #del  freq_L1, asd_L1, freq_V1, asd_V1, freq_K1, asd_K1 
 
---
+++ read_sqlite_xml_event.py
@@ -0,0 +1,67 @@
+## Sqlite
+from ligo.skymap.util import sqlite
+
+## Fits files 
+from ligo.skymap.io import read_sky_map
+
+## XML
+from gwpy.table import Table as gwpy_Table
+
+##Read .fits files
+# for more please see this,  https://lscsoft.docs.ligo.org/ligo.skymap/io/fits.html
+#fits_data =  read_sky_map('0.fits')
+
+
+
+
+## Read sqlite , some examples 
+#with sqlite.open('events.sqlite', 'r') as db:
+    ##print(sqlite.get_filename(db))
+    # Get simulated rate from LIGO-LW process table
+#    (rate,), = db.execute('SELECT comment FROM process WHERE program = ?', ('bayestar-inject',))
+
+     # Get simulated detector network from LIGO-LW process table
+    #(network,), = db.execute('SELECT ifos FROM process WHERE program = ?', ('bayestar-realize-coincs',))
+    
+    # Get number of Monte Carlo samples from LIGO-LW process_params table
+   # (nsamples,), = db.execute('SELECT value FROM process_params WHERE program = ? AND param = ?', ('bayestar-inject', '--nsamples'))
+    
+
+
+
+
+## For XML inside the 'events' folder 
+# ``tablename=`` keyword argument. The following tables were found: 'coinc_definer', 'process_params', 'process', 'time_slide', 'coinc_event', 'coinc_event_map', 'sngl_inspiral'
+
+#but most of parameters you need should be here, I think
+xml_data = gwpy_Table.read('events.xml.gz', format="ligolw", tablename="sim_inspiral")
+
+fits_geocent_end_time_nsdata = read_sky_map('allsky/1.fits')
+
+gps_time = fits_data[1]['gps_time']
+
+geocent_end_time_ns =xml_data[1]['']
+
+geocent_end_time =xml_data[1]['geocent_end_time']
+
+
+In [50]: fits_data =  read_sky_map('allsky/1.fits')
+
+In [51]: gps_time = fits_data[1]['gps_time']
+
+In [52]: geocent_end_time_ns =xml_data[1]['geocent_end_time_ns']
+
+In [53]: geocent_end_time =xml_data[1]['geocent_end_time']
+
+In [54]: gps_time
+Out[54]: 1009476826.340861
+
+In [55]: geocent_end_time
+Out[55]: 1009476826
+
+In [56]: geocent_end_time_ns
+Out[56]: 351013541
+
+In [57]: gps_time /(geocent_end_time + geocent_end_time_ns*10**-9)
+Out[57]: 0.9999999999899427
+
